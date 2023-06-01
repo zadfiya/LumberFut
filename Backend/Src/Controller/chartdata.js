9ade359b9513079410d154bd596a16d27b71bee3
Naren Zadafiya <narenzadfiya3579@gmail.com>
2023-05-31 22:23:34 -0400

    function is working perfectly

--- Backend/Src/Controller/chartdata.js
+++ Backend/Src/Controller/chartdata.js
@@ -1,7 +1,24 @@
+const XLSX = require('xlsx');
+const path = require('path');
+var file_path = path.resolve(__dirname,'../../public/CSV_Files/LumberFut.xlsx' )
 
 
 const readCSVData = ()=>{
+    const workbook = XLSX.readFile(file_path);
 
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+    const jsonData = XLSX.utils.sheet_to_json(worksheet);
+
+    console.log(jsonData);
+  //   fs.createReadStream(file_path)
+  // .pipe(csv())
+  // .on('data', (row) => {
+  //   console.log(row);
+  // })
+  // .on('end', () => {
+  //   console.log('CSV file successfully processed');
+  // });
 }
 
 module.exports ={readCSVData}
--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var app = express()
 var cors = require('cors')
+var {readCSVData} = require("./Src/Controller/chartdata")
 app.use(cors)
 const PORT = process.env.port || 3001
 app.listen(PORT,()=>{
     console.log("Lumberfut Server is running on the port: "+PORT)
+    readCSVData();
 })
