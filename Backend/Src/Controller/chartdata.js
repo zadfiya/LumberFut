1c8820e0d5c109817ff259cf90c346e7dd94f7c7
Naren Zadafiya <narenzadfiya3579@gmail.com>
2023-05-31 23:28:29 -0400

    function for saving stock data created

--- Backend/Src/Controller/chartdata.js
+++ Backend/Src/Controller/chartdata.js
@@ -1,8 +1,8 @@
 const XLSX = require('xlsx');
 const path = require('path');
-var file_path = path.resolve(__dirname,'../../public/CSV_Files/LumberFut.xlsx' )
-const createTableQuery = `
-CREATE TABLE IF NOT EXISTS stocks (
+var file_path = path.resolve(__dirname,'../../public/CSV_Files/LumberFut.xlsx' );
+const db = require("../DB/db")
+const createTableQuery = `CREATE TABLE IF NOT EXISTS stock_data (
   Date TEXT,
   Open REAL,
   High REAL,
@@ -12,14 +12,16 @@ CREATE TABLE IF NOT EXISTS stocks (
   Volume REAL
 )`;
 
-const readCSVData = (req,res,next)=>{
+const readCSVData = async ()=>{
     const workbook = XLSX.readFile(file_path);
 
     const sheetName = workbook.SheetNames[0];
     const worksheet = workbook.Sheets[sheetName];
     const jsonData = XLSX.utils.sheet_to_json(worksheet);
 
-    res.status(200).json(jsonData);
+   await saveDataDB((jsonData));
+    //return jsonData;
+    //res.status(200).json(jsonData);
   //   fs.createReadStream(file_path)
   // .pipe(csv())
   // .on('data', (row) => {
@@ -30,8 +32,32 @@ const readCSVData = (req,res,next)=>{
   // });
 }
 
-const saveDataDB = ()=>{
+const saveDataDB = (jsonData)=>{
+    db.run(createTableQuery, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Table created successfully or already exists.');
+  const keys = [ 'Date', 'Open', 'High', 'Low', 'Close', 'AdjClose', 'Volume' ]
 
+
+    // console.log(Object.keys(jsonData[0]));
+  jsonData.forEach(async (row) => {
+    let placeholders = Object.values(row).map(() => '?').join(',');
+
+    let sql = `INSERT INTO stock_data(${keys.join(',')}) VALUES(${placeholders})`;
+
+   await db.run(sql, Object.values(row), function(err) {
+      if (err) {
+        return console.error(err.message);
+      }
+      console.log(`Row inserted with rowid ${this.lastID}`);
+    });
+  });
+});
 }
 
+const getCSVData = (req,res,next)=>{
+
+}
 module.exports ={readCSVData}
--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,14 @@
 var express = require('express')
 var app = express()
 var cors = require('cors')
-
+var {readCSVData} = require("./Src/Controller/chartdata")
 app.use(cors)
 
 var router = require("./Src/Route/index");
 app.use(router);
 
 const PORT = process.env.port || 3001;
-app.listen(PORT,()=>{
+app.listen(PORT,async ()=>{
     console.log("Lumberfut Server is running on the port: "+PORT)
+    await readCSVData();
 })
