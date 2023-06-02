633045572e2bac6776aed217447c9ea5e11f1c0e
Naren Zadafiya <narenzadfiya3579@gmail.com>
2023-06-01 20:23:53 -0400

    Dropdown menu for feature selection

--- Frontend/src/Component/CSV_Reader/CSV_Reader.js
+++ Frontend/src/Component/CSV_Reader/CSV_Reader.js
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Line} from "chart.js"
-const Chart=()=>{
 
+
+
+
+const Chart=()=>{
+    const handleOptionChange = (e) => {
+        console.log(e.target.value)
+      };
+    return (
+        <>
+            <select value={"Open"} onChange={handleOptionChange}>
+                <option value="Open">Open</option>
+                <option value="High">High</option>
+                <option value="Low">Low</option>
+                <option value="Close">Close</option>
+                <option value="AdjClose">Adj Close</option>
+                <option value="Volume">Volume</option>
+            </select>
+        </>
+    )
 }
